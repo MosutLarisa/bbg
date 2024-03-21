d2db629c001f549bdf8ad0ed216b7f6cc262bed6
Andreea Suciu <andreea.suciu@emanuel.ro>
2024-03-21 13:04:00 +0200

    adauga fata

--- script.js
+++ script.js
@@ -2,6 +2,41 @@ function setup() {
     createCanvas(800, 600);
 }
 
-function draw() {
 
+
+function draw() {
+    background("gray");
+    smileyfaceAndreea();
 }
+
+
+    function smileyfaceAndreea(){
+    //body
+    fill("black");
+    stroke("blue");
+    circle(400, 400, 50);
+    
+    //eyes
+    
+    stroke("purple");
+    ellipse(390, 295, 10, 5);
+    ellipse(410, 295, 10, 5);
+    //eye pupils
+    stroke("black");
+    fill("black")
+    circle(390, 295, 3);
+    circle(410, 295, 3);
+
+    //mouth
+    stroke("pink");
+    fill("black");
+    arc(400, 310, 20, 7, 0, Math.PI);
+
+    //cheeks
+    stroke("red")
+    fill("pink");
+    circle(420, 305, 7);
+    circle(380, 305, 7);
+    }
+
+
