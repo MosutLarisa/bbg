553002518c7ff6b1c2c54da8ecc35f1bdf881651
amarispurice <amaris.purice@emmanuel.ro>
2024-03-21 12:45:29 +0200

    am adaugat fata lui amaris

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(700, 500);
 }
 
 function draw() {
+  background("brown");
+  smilyFaceAmaris()
+}
+function smilyFaceAmaris(){
+  //body
+  fill("white");
+  stroke("black");
+  circle(300, 300, 50);
 
+  //eyes
+ // circle( 390, 295, 5);
+  //circle( 410, 295, 5);
+    stroke("green");
+    ellipse(290, 295, 10, 5);
+    ellipse(310, 295, 10, 5);
+//eye pupils
+    stroke("black");
+    fill("black");
+    circle( 310, 295, 3);
+    circle( 290, 295, 3);
+//mounth
+    stroke("red");
+    fill("black");
+    arc(300, 310, 20, 5, 0, Math.PI);
 }
