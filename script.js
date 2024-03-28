5d1ac475a6e0b5c14b0f1056d6beda03384e5fdd
Larisa Moșuț <larisa.mosut@emanuel.ro>
2024-03-28 11:50:14 +0200

    am pus coordonate x si y

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function draw() {
     background("gray");
     smileyfaceAndreea();
     smilyFaceAmaris()
-    smilyFaceLari();
+    smilyFaceLari(400, 300);
 }
 
 
@@ -68,33 +68,33 @@ function smilyFaceAmaris(){
     arc(300, 310, 20, 5, 0, Math.PI);
 }
    
-function smilyFaceLari(){
+function smilyFaceLari(x,y){
         //body
         fill("white");
         stroke("black");
-        circle(400, 300, 50);
+        circle(x, y, 50);
         
         //eyes
         //circle(410, 295, 5);
         stroke("blue");
-        ellipse(390, 295, 10, 5);
-        ellipse(410, 295, 10, 5);
+        ellipse(x-10, y-5, 10, 5);
+        ellipse(x+10, y-5, 10, 5);
         //eye pupils
         stroke("black");
         fill("black")
-        circle(390, 295, 3);
-        circle(410, 295, 3);
+        circle(x-10, y-5, 3);
+        circle(x+10, y-5, 3);
     
         //mouth
         stroke("red");
         fill("black");
-        arc(400, 310, 20, 7, 0, Math.PI);
+        arc(x, y+10 , 20, 7, 0, Math.PI);
     
         //cheeks
         stroke("pink")
         fill("pink");
-        circle(420, 305, 7);
-        circle(380, 305, 7);
+        circle(x+20, y+5, 7);
+        circle(x-20,y+5, 7);
     
     
 }
