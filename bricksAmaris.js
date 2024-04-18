2f32027f3ce0fc60452b1052ececf3ef177db0d6
amarispurice <amaris.purice@emmanuel.ro>
2024-04-18 11:20:43 +0300

    bricksul meu

--- bricksAmaris.js
+++ bricksAmaris.js
@@ -1,18 +1,34 @@
 let bricksAmaris=[];
 
-const bricksRowsAmaris = 1;
-const brickClosAmaris = 1;
-const rowBricksNumberAmaris = 4;
+const marginAmaris = 10;
+const distanceBetweenBricksAmaris=10 ;
+
+const bricksRowsAmaris = 3;
+const rowBricksNumberAmaris = 7;
+
+const bricksWidthAmaris =Math.floor(
+    (tableWidth- 
+        marginAmaris*2 - 
+        (rowBricksNumberAmaris-1)
+        *distanceBetweenBricksAmaris)/rowBricksNumberAmaris);
 
 function initBricksAmaris(){
+    let x= marginAmaris;
     for(let i=0; i < bricksRowsAmaris * rowBricksNumberAmaris; i++){
+        const row = Math.floor(i/rowBricksNumberAmaris)+1 ;
+        
+        x= x+ bricksWidthAmaris+ distanceBetweenBricksAmaris;
+        if(i % rowBricksNumberAmaris==0) 
+            x=marginAmaris;
+
         bricksAmaris.push({
             hit: false,
-            x: i*70,
-            y: 50,
+            x,
+            y: row * 35,
             color: "#D8155F", 
-            width: 50,
+            width: bricksWidthAmaris,
             height: 10,
+            row
         })
     }
 }
